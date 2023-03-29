b9b9df000c3e301fd5728b2c1304a64dd26cd6f2
radialcode-mohit <mohitgirdhar9034@gmail.com>
2023-03-29 09:39:27 +0530

    font-family-import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import LongTerm from "./components/LongTerm";
 function App() {
-  return <></>;
+  return (
+    <>
+      <LongTerm />
+    </>
+  );
 }
 
 export default App;
